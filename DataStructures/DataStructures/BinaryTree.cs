c908797c6a9cdf9ce9f9bd96a82069e42b2786b2
Volodymyr Koshkarov <brealx@gmail.com>
2018-07-24 08:10:35 -0400

    Added Binary Tree data strucure and partially covered methods with tests

---
+++ DataStructures/DataStructures/BinaryTree.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DataStructures
+{
+    public class BinaryTree
+    {
+        public BinaryTreeNode<int> Root { get; set; }
+
+        public BinaryTree()
+        {
+            Root = null;
+        }
+
+        public BinaryTree(BinaryTreeNode<int> root)
+        {
+            Root = root;
+        }
+
+        public void Clear()
+        {
+            Root = null;
+        }
+
+        public bool Contains(int key)
+        {
+            return Search(Root, key) != null;
+        }
+
+        public BinaryTreeNode<int> Search(BinaryTreeNode<int> node, int key)
+        {
+            if (node == null || node.Value == key)
+                return node;
+            if (key > node.Value)
+                return Search(node.Right, key);
+            if (key < node.Value)
+                return Search(node.Left, key);
+
+            return node;
+        }
+
+        public BinaryTreeNode<int> Insert(BinaryTreeNode<int> root, int key)
+        {
+            return default(BinaryTreeNode<int>);
+        }
+
+        public void Delete()
+        {
+            
+        }
+
+        public List<int> TraverseInOrder()
+        {
+            List<int> list = new List<int>();
+            InOrderTraversal(Root, list);
+            return list;
+        }
+
+        private static void InOrderTraversal(BinaryTreeNode<int> node, List<int> list)
+        {
+            if (node == null) return;
+            InOrderTraversal(node.Left, list);
+            list.Add(node.Value);
+            InOrderTraversal(node.Right, list);
+        }
+
+        private static void PreOrderTraversal(BinaryTreeNode<int> node, List<int> list)
+        {
+            if (node == null) return;
+            list.Add(node.Value);
+            PreOrderTraversal(node.Left, list);
+            PreOrderTraversal(node.Right, list);
+        }
+
+        public bool IsBinarySearchTreeTraversal()
+        {
+            return IsBstTraversal(Root);
+        }
+        public bool IsBinarySearchTree()
+        {
+            return IsBst(Root, int.MinValue, int.MaxValue);
+        }
+
+
+
+        private static bool IsBst(BinaryTreeNode<int> node, int min, int max)
+        {
+            if (node == null)
+                return true;
+            if (node.Value < min || node.Value > max)
+                return false;
+
+            // node.Value+1 and node.Value-1 are done to allow only distinct elements in BST.
+            return IsBst(node.Left, min, node.Value - 1) &&
+                   IsBst(node.Right, node.Value + 1, max);
+        }
+
+        private static bool IsBstTraversal(BinaryTreeNode<int> node)
+        {
+            var list = new List<int>();
+            InOrderTraversal(node, list);
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i - 1] > list[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
+
---
+++ DataStructures/Tests/BinarySearchTreeTests.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace DataStructures.Tests
+{
+    [TestFixture]
+    class BinarySearchTreeTests
+    {
+        BinaryTree bstValid;
+        BinaryTree bstInvalid;
+
+        [SetUp]
+        protected void SetUp()
+        {
+            // Valid Binary Search Tree
+            bstValid = new BinaryTree();
+            bstValid.Root = new BinaryTreeNode<int>(4);
+            bstValid.Root.Left = new BinaryTreeNode<int>(2);
+            bstValid.Root.Left.Left = new BinaryTreeNode<int>(1);
+            bstValid.Root.Left.Right = new BinaryTreeNode<int>(3);
+            bstValid.Root.Right = new BinaryTreeNode<int>(6);
+            bstValid.Root.Right.Right = new BinaryTreeNode<int>(7);
+            bstValid.Root.Right.Left = new BinaryTreeNode<int>(5);
+
+            // Invlid Binary Search Tree
+            bstInvalid = new BinaryTree();
+            bstInvalid.Root = new BinaryTreeNode<int>(3);
+            bstInvalid.Root.Left = new BinaryTreeNode<int>(2);
+            bstInvalid.Root.Left.Left = new BinaryTreeNode<int>(1);
+            bstInvalid.Root.Right = new BinaryTreeNode<int>(5);
+            bstInvalid.Root.Right.Right = new BinaryTreeNode<int>(1);
+            bstInvalid.Root.Right.Left = new BinaryTreeNode<int>(6);
+        }
+
+
+        [Test]
+        public void IsBinarySearchTreeTraversalValid()
+        {
+            Assert.AreEqual(true, bstValid.IsBinarySearchTreeTraversal());
+        }
+
+        [Test]
+        public void IsBinarySearchTreeTraversalInvalid()
+        {
+            Assert.AreEqual(false, bstInvalid.IsBinarySearchTreeTraversal());
+        }
+
+        [Test]
+        public void IsBinarySearchTreeValid()
+        {
+            Assert.AreEqual(true, bstValid.IsBinarySearchTree());
+        }
+
+        [Test]
+        public void IsBinarySearchTreeInvalid()
+        {
+            Assert.AreEqual(false, bstInvalid.IsBinarySearchTree());
+        }
+
+        [Test]
+        public void ContainsKey()
+        {
+            for (int i = 1; i < 8; i++)
+            {
+                Assert.AreEqual(true, bstValid.Contains(i));
+            }
+        }
+
+        [Test]
+        public void DoesNotContainKey()
+        {
+            Assert.AreEqual(false, bstValid.Contains(8));
+
+        }
+
+    }
+}
