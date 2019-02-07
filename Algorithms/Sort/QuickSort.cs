1adc209d74a1d19f2abb5ec87f431fe0b59fd3f3
Volodymyr Koshkarov <brealx@gmail.com>
2019-02-07 13:36:35 -0500

    Updated quick test with Robert Sedgewick implementation.

--- Algorithms/Sort/QuickSort.cs
+++ Algorithms/Sort/QuickSort.cs
@@ -1,45 +1,69 @@
-using System;
+﻿using System;
+using Algorithms.Extensions;
 
 namespace Algorithms.Sort
 {
+    /// <summary>
+    /// Quicksort (sometimes called partition-exchange sort) is an O(N log N) efficient sorting algorithm,
+    /// serving as a systematic method for placing the elements of an array in order.
+    ///
+    /// Stable: false
+    /// 
+    /// Time complexity:
+    /// Best: Ω(n log(n))
+    /// Average: Θ(n log(n))
+    /// Worst: O(n^2)
+    ///
+    /// Space Complexity:
+    /// Worst: O(log(n))
+    /// </summary>
     public class QuickSort
     {
         private static readonly Random Random = new Random();
 
-        public static T[] Sort<T>(T[] arr) where T : IComparable<T>
+        public static void Sort<T>(T[] arr) where T : IComparable<T>
         {
+            new Random().Shuffle(arr);
             Sort(arr, 0, arr.Length - 1);
-            return arr;
         }
 
-        private static void Sort<T>(T[] arr, int start, int finish) where T : IComparable<T>
+        private static void Sort<T>(T[] arr, int lo, int hi) where T : IComparable<T>
         {
-            int s = start;
-            int f = finish;
-            T pivot = arr[start + Random.Next(finish - start)];
+            if (lo < hi)
+            {
+                int j = Partition(arr, lo, hi);
 
-            while (s <= f)
-            {
-                while (arr[s].CompareTo(pivot) < 0)
-                    s++;
-                while (arr[f].CompareTo(pivot) > 0)
-                    f--;
-                if (s <= f)
-                {
-                    (arr[s], arr[f]) = (arr[f], arr[s]);
-                    s++;
-                    f--;
-                }
+                Sort(arr, lo, j - 1);
+                Sort(arr, j + 1, hi);
             }
-            if (start < f)
-            {
-                Sort(arr, start, f);
-            }
-            if (s < finish)
-            {
-                Sort(arr, s, finish);
-            }
+
+            
         }
 
+        private static int Partition<T>(T[] arr, int lo, int hi) where T : IComparable<T>
+        {
+            int i = lo;
+            int j = hi+1;
+
+            T pivot = arr[lo];
+
+            while (true)
+            {
+                while (arr[++i].CompareTo(pivot) < 0)
+                    if (i == hi)
+                        break;
+                while (pivot.CompareTo(arr[--j]) < 0)
+                    if (j == lo)
+                        break;
+
+                if (i >= j)
+                    break;
+
+                (arr[i], arr[j]) = (arr[j], arr[i]);
+            }
+
+            (arr[lo], arr[j]) = (arr[j], arr[lo]);
+            return j;
+        }
     }
 }
